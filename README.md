fda2e95110d553ca00063beb0ca161bba3c85cc4
lama1ftl <vsevolod.radionov@gmail.com>
2022-10-17 15:24:55 +0700

    Added the ability to save files in the original extension.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-COMING SOON
+# BG_REMOVER
+
+___
+
+### ABOUT
+
+A small script to remove the background from the photo of objects.
+___
+
+<details>
+<summary>EXAMPLES</summary>
+<p>
+
+| Before                                  | After                                              |
+|-----------------------------------------|----------------------------------------------------|
+| ![THIS IS AN IMAGE](input/example.jpg)  | ![THIS IS AN IMAGE](output/example_formatted.png)  |
+| ![THIS IS AN IMAGE](input/example2.jpg) | ![THIS IS AN IMAGE](output/example2_formatted.png) |
+
+</p>
+</details>
--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from PIL import Image
 from pathlib import Path
 import logging
 
-
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 
-def remove_bg():
+def remove_bg(param):
     list_of_extensions = ['*.png', '*.jpg', '*.jpeg', '*.PNG', '*.JPG', '*.JPEG']
     all_imgs = []
 
@@ -21,10 +20,23 @@ def remove_bg():
 
         input_img = Image.open(input_path)
         output_img = remove(input_img)
+
+        if param == 2 and input_img.mode not in ('RGBA', 'LA'):
+            img_suffix = input_path.suffix
+            output_path = f'output/{img_name}_formatted{img_suffix}'
+            bg = Image.new('RGB', input_img.size, (255, 255, 255))
+            bg.paste(output_img, output_img.split()[-1])
+            output_img = bg
+
         output_img.save(output_path)
 
-        logging.info(f'Formatted: {index+1}/{len(all_imgs)}')
+        logging.info(f'Formatted: {index + 1}/{len(all_imgs)}')
+
+
+def main():
+    print('Convert final images to png or keep original extensions?\n[1 - png|2 - original]')
+    return remove_bg(1 if input() == '1' else 2)
 
 
 if __name__ == '__main__':
-    remove_bg()
+    main()
